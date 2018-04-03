8a7d1466fcaeab3516eae2cec821579689d629bb
chrisperk <christopherperkins.512@gmail.com>
2018-04-03 17:07:02 -0500

    added config to gitignore file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { YOUTUBE_API_KEY } from './config';
+
+import SearchBar from './components/search-bar';
 
 // Create a new component. This component should produce some HTML.
-const App = () => <div>hi</div>;
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
 
 // Take this component's generated HTML and put it on the page.
 ReactDOM.render(<App />, document.querySelector('.container'));
