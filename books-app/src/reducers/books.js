f6b644f65a361be30378b47ffdc6c33211b303df
chrisperk <christopherperkins.512@gmail.com>
2018-04-16 16:40:28 -0500

    connected BookDetail component to redux store

--- books-app/src/reducers/books.js
+++ books-app/src/reducers/books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'Javascript' },
-        { title: 'Harry Potter' },
-        { title: 'The Dark Tower' },
-        { title: 'Eloquent Ruby' }                
+        { title: 'Javascript', pages: 101 },
+        { title: 'Harry Potter', pages: 600 },
+        { title: 'The Dark Tower', pages: 250 },
+        { title: 'Eloquent Ruby', pages: 5 }             
     ];
 }
