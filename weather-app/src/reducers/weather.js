e27f36e64381810a5a726a84d754b5620cd3dfa4
chrisperk <christopherperkins.512@gmail.com>
2018-04-19 14:50:27 -0500

    modified weather reducer to return array of searched cities as its state

--- weather-app/src/reducers/weather.js
+++ weather-app/src/reducers/weather.js
@@ -1,4 +1,11 @@
-export default function (state = null, action) {
-  console.log('action received', action);
-  return state;
+import { FETCH_WEATHER } from '../actions';
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [action.payload.data, ...state];
+
+    default:
+      return state;
+  }
 }
