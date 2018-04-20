cf733b3d33d3d56473edcb8116561dacde4caa7c
chrisperk <christopherperkins.512@gmail.com>
2018-04-20 15:04:01 -0500

    added basic routing example

--- blog-app/src/index.js
+++ blog-app/src/index.js
@@ -1,15 +1,29 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
+// import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+const Hello = () => <div>Hello!</div>;
+
+const Goodbye = () => <div>Goodbye!</div>;
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        Header
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+        Footer
+      </div>
+    </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container'),
+);
