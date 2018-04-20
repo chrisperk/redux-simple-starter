61f3eaa95e654bcddd75e6ebe000008a0faa753d
chrisperk <christopherperkins.512@gmail.com>
2018-04-19 19:42:37 -0500

    added google maps integration to visualize city

---
+++ weather-app/src/components/google-map.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class GoogleMap extends Component {
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon,
+      },
+    });
+  }
+
+  render() {
+    return (
+      // refer to html element with this.refs.map
+      <div ref="map" />
+    );
+  }
+}
+
+GoogleMap.propTypes = {
+  lat: PropTypes.number.isRequired,
+  lon: PropTypes.number.isRequired,
+};
+
+export default GoogleMap;
--- weather-app/src/containers/weather-list.js
+++ weather-app/src/containers/weather-list.js
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google-map';
 
 const renderWeather = (city) => {
   const temps = city.list.map(weather => weather.main.temp);
   const pressures = city.list.map(weather => weather.main.pressure);
   const humidities = city.list.map(weather => weather.main.humidity);
-
+  const { lon, lat } = city.city.coord;
 
   return (
     <tr key={city.city.id}>
-      <td>{city.city.name}</td>
+      <td><GoogleMap lat={lat} lon={lon} /></td>
       <td>
         <Chart data={temps} color="red" units="K" />
       </td>
