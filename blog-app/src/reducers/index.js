74e235e0c1c99b20f2cae4035defda288acf5f7a
chrisperk <christopherperkins.512@gmail.com>
2018-04-21 14:48:55 -0500

    added redux-form pkg and added form reducer to root reducer

--- blog-app/src/reducers/index.js
+++ blog-app/src/reducers/index.js
@@ -1,8 +1,10 @@
+import { reducer as formReducer } from 'redux-form';
 import { combineReducers } from 'redux';
 import PostsReducer from './posts';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
