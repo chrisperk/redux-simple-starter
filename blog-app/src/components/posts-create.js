b83575edd96fa24bc249696101290c9a25f6d400
chrisperk <christopherperkins.512@gmail.com>
2018-04-21 17:32:27 -0500

    generalized renderField function to render all fields in our redux form

--- blog-app/src/components/posts-create.js
+++ blog-app/src/components/posts-create.js
@@ -3,12 +3,43 @@ import { Link } from 'react-router';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsCreate extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label htmlFor={field.name}>
+          {field.label}
+          <input
+            className="form-control"
+            name={field.name}
+            id={field.name}
+            type={field.type}
+            {...field.input}
+          />
+        </label>
+      </div>
+    );
+  }
+
   render() {
     return (
       <form>
         <Field
+          label="Title"
+          type="text"
           name="title"
-          component={}
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          type="text"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          type="text"
+          name="content"
+          component={this.renderField}
         />
       </form>
     );
