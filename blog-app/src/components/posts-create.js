495a64c3518dab9daea15bdc8ecc3549719db152
chrisperk <christopherperkins.512@gmail.com>
2018-04-21 17:49:36 -0500

    added validation checks to post create form

--- blog-app/src/components/posts-create.js
+++ blog-app/src/components/posts-create.js
@@ -1,51 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 import { Field, reduxForm } from 'redux-form';
 
-class PostsCreate extends Component {
-  renderField(field) {
-    return (
-      <div className="form-group">
-        <label htmlFor={field.name}>
-          {field.label}
-          <input
-            className="form-control"
-            name={field.name}
-            id={field.name}
-            type={field.type}
-            {...field.input}
-          />
-        </label>
-      </div>
-    );
-  }
-
-  render() {
-    return (
-      <form>
-        <Field
-          label="Title"
-          type="text"
-          name="title"
-          component={this.renderField}
+function renderField(field) {
+  return (
+    <div className="form-group">
+      <label htmlFor={field.name}>
+        {field.label}
+        <input
+          className="form-control"
+          name={field.name}
+          id={field.name}
+          type={field.type}
+          {...field.input}
         />
-        <Field
-          label="Tags"
-          type="text"
-          name="tags"
-          component={this.renderField}
-        />
-        <Field
-          label="Post Content"
-          type="text"
-          name="content"
-          component={this.renderField}
-        />
-      </form>
-    );
-  }
+      </label>
+    </div>
+  );
 }
 
+function validate(values) {
+  // Create empty object to append errors to.
+  const errors = {};
+
+  // Validate the inputs from values param.
+  if (!values.title || values.titles.length < 3) {
+    errors.title = 'Enter a title that is at least 3 characters!';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter some categories!';
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter some content!';
+  }
+
+  // If errors object is empty, the form is fine to submit.
+  // If errors has any properties, redux-form assumes form is invalid and will not submit.
+  return errors;
+}
+
+const PostsCreate = () =>
+  (
+    <form>
+      <Field
+        label="Title"
+        type="text"
+        name="title"
+        component={renderField}
+      />
+      <Field
+        label="Categories"
+        type="text"
+        name="categories"
+        component={renderField}
+      />
+      <Field
+        label="Post Content"
+        type="text"
+        name="content"
+        component={renderField}
+      />
+    </form>
+  );
+
 export default reduxForm({
+  validate,
   form: 'PostsCreateForm',
 })(PostsCreate);
